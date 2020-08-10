3483e2e9ded020a15048f9e4f3da1ebdf48b16bd
Angelo Mosquera <anggelomos@outlook.com>
2020-08-10 11:48:27 -0500

    feat(DataRetriever): Add class data retriever with the function productivity_time

--- data_retreiver.py
+++ data_retreiver.py
@@ -1,22 +1,30 @@
 import requests
+from datetime import date
 
-raw_data = requests.get('https://www.rescuetime.com/anapi/data?key=B63SG1dvEUH_h5YZAus3mZLlTzKUyvlt5_KBmyhZ&perspective=interval&restrict_kind=productivity&interval=day&restrict_begin=2020-08-01&restrict_end=2020-08-01&format=json')
-raw_productivity_data = raw_data.json()["rows"]
-productivity_time = {"very productive":0, "productive": 0, "neutral": 0, "distracting": 0, "very distracting": 0}
-for data in raw_productivity_data:
-    category = data[3]
-    time = data[1]
-    if category == 2:
-        productivity_time["very productive"] = time
-    elif category == 1:
-        productivity_time["productive"] = time
-    elif category == 0:
-        productivity_time["neutral"] = time
-    elif category == -1:
-        productivity_time["distracting"] = time
-    elif category == -2:
-        productivity_time["very distracting"] = time
-    else:
-        raise IndexError("Wrong productivity category")
+class  DataRetreiver:
+    def __init__(self, rescuetime_key):
+        self.rescuetime_key = rescuetime_key
 
-print(productivity_time)
+    current_date = str(date.today())
+
+    def productivity_time(self, date: str=current_date):
+        raw_data = requests.get(f"https://www.rescuetime.com/anapi/data?key={self.rescuetime_key}&perspective=interval&restrict_kind=productivity&interval=day&restrict_begin={date}&restrict_end={date}&format=json")
+        raw_productivity_data = raw_data.json()["rows"]
+        productivity_time = {"very productive":0, "productive": 0, "neutral": 0, "distracting": 0, "very distracting": 0}
+        for data in raw_productivity_data:
+            category = data[3]
+            time = data[1]
+            if category == 2:
+                productivity_time["very productive"] = time
+            elif category == 1:
+                productivity_time["productive"] = time
+            elif category == 0:
+                productivity_time["neutral"] = time
+            elif category == -1:
+                productivity_time["distracting"] = time
+            elif category == -2:
+                productivity_time["very distracting"] = time
+            else:
+                raise IndexError("Wrong productivity category")
+        return productivity_time
+#print(productivity_time)
