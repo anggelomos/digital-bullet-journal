a7e6729583e1caf49217ab2e6c4358e59214f60d
Angelo Mosquera <anggelomos@outlook.com>
2020-08-11 12:14:09 -0500

    test(read): Add test for the read function

--- test_sheets_manager.py
+++ test_sheets_manager.py
@@ -11,5 +11,10 @@ class DataRetrieverTest(unittest.TestCase):
         for index, case in enumerate(testcase.values()):
             self.assertEqual(expected[index], case)
 
+    def test_read(self):
+        testcase = sheet_test.read("A1:B2")
+        expected = [["Hola", "Value"], [1.0, 2.0]]
+        self.assertEqual(expected, testcase)
+
 if __name__ == "__main__":
     unittest.main()
